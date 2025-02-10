b6549c4e93024a4437d63067a2fdc1c2278b1d3f
Brijraj Menor <89831450+brijrajmenor@users.noreply.github.com>
2025-02-11 03:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 # Initialize Firebase if not already initialized
 import json
 key_dict = json.loads(st.secrets["textkey"])
-cred = service_account.Credentials.from_service_account_info(key_dict)
+cred = Credentials.certificate(key_dict)
 firebase_admin.initialize_app(cred)
 
 # Initialize Firestore client
